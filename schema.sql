079b0c47847ff4df65eced057550771df52c2295
Muendem Fonkeng <fonkengmuendem@yahoo.com>
2020-08-03 16:17:05 -0500

    added primary keys to role and employee tables

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ DROP DATABASE IF EXISTS employeeTracker_db;
 
 CREATE DATABASE employeeTracker_db;
 
-USE DATABASE employeeTracker_db;
+USE employeeTracker_db;
 
 CREATE TABLE department (
    id INTEGER(20) AUTO_INCREMENT NOT NULL,
@@ -14,13 +14,15 @@ CREATE TABLE role (
    id INTEGER(20) AUTO_INCREMENT NOT NULL,
    title VARCHAR(30) NULL,
    salary DECIMAL(10,4) NULL,
-   department_id INTEGER(20)
+   department_id INTEGER(20),
+   PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-   id INTEGER(20) AUTO INCREMENT NOT NULL,
+   id INTEGER(20) AUTO_INCREMENT NOT NULL,
    first_name VARCHAR(30) NULL,
    last_name VARCHAR(30) NULL,
    role_id INTEGER NULL,
-   manager_id INTEGER NULL
+   manager_id INTEGER NULL,
+   PRIMARY KEY (id)
 );
