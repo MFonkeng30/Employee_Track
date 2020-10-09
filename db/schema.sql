f6a915529aed00087c3f442ee55c2282007cd897
Muendem Fonkeng <fonkengmuendem@yahoo.com>
2020-10-08 20:08:48 -0500

    added changes to table values

--- db/schema.sql
+++ db/schema.sql
@@ -6,23 +6,23 @@ USE employeeTracker_db;
 
 CREATE TABLE department (
    id INTEGER(20) AUTO_INCREMENT NOT NULL,
-   name VARCHAR(30) NULL,
+   name VARCHAR(30) NOT NULL,
    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
    id INTEGER(20) AUTO_INCREMENT NOT NULL,
-   title VARCHAR(30) NULL,
-   salary DECIMAL(10,4) NULL,
-   department_id INTEGER(20),
+   title VARCHAR(30) NOT NULL,
+   salary DECIMAL(10,4) NOT NULL,
+   department_id INTEGER(20) NOT NULL,
    PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
    id INTEGER(20) AUTO_INCREMENT NOT NULL,
-   first_name VARCHAR(30) NULL,
-   last_name VARCHAR(30) NULL,
-   role_id INTEGER NULL,
-   manager_id INTEGER NULL,
+   first_name VARCHAR(30) NOT NULL,
+   last_name VARCHAR(30) NOT NULL,
+   role_id INTEGER NOT NULL,
+   manager_id INTEGER,
    PRIMARY KEY (id)
 );
