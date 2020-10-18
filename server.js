202b747d390d553fd7d8457f552a73691fd1e4cb
Muendem Fonkeng <fonkengmuendem@yahoo.com>
2020-10-18 09:23:30 -0500

    added start function

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var inquirer = require("inquirer");
 var table = require("console.table");
 var mysql = require("mysql");
+const { start } = require("repl");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -16,5 +17,46 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err
     console.log("connected as id " + connection.threadId)
-    connection.end();
+    console.log("Hello")
+    start();
 });
+
+function start() [
+    inquirer
+    .prompt([
+        name: "task",
+        type: "list",
+        message: "What would you like to do?"
+        choices: [
+            "Add Employee, Role, or Department"
+            "View all Employees or Departments"
+            "Update Roles or Managers"
+            "Delete Employee"
+            "Exit" 
+        ]
+    ])
+    .then(function(answer) {
+        switch (answer.task) {
+            case "Add Employee, Role, or Department":
+               addSomething();
+               break;
+           
+            case "View all Employees or Departments": 
+               viewSomething();
+               break;
+   
+            case "Update Roles or Managers":
+                updateSomething();
+                break;
+   
+            case "Delete Employee":
+                deleteEmployee();
+                break;
+   
+            case "Exit":
+                console.log("Connection Terminated!  Goodbye!")
+                connection.end();
+                
+        }
+    })
+]
